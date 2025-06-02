c37149b92ed097bfcb8f71fa95f69bec80245378
user <anonymous@gmail.com>
2025-06-02 16:56:38 +0700

    - Manage Hidden Item in Map

--- Assets/_Assets/Scripts/Gameplay/HiddenItem/HiddenItem.cs
+++ Assets/_Assets/Scripts/Gameplay/HiddenItem/HiddenItem.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
-public class HiddenItem : MonoBehaviour
+public class HiddenItem : MonoBehaviour, ISelectable
 {
     [SerializeField] private HiddenItemInfo hiddenItemInfo;
+
+    public void Select()
+    {
+        var itemID = hiddenItemInfo.ItemID;
+        Debug.Log($"--- (ITEM) Select at item with ID: {itemID}");
+    }
 }
--- Assets/_Assets/Scripts/Gameplay/Map/MapItemManager.cs
+++ Assets/_Assets/Scripts/Gameplay/Map/MapItemManager.cs
@@ -1,8 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapItemManager : MonoBehaviour
 {
     [SerializeField] private List<HiddenItem> hiddenItemInMap = new List<HiddenItem>();
+
+    private void Reset()
+    {
+        GetAllHiddenItemInMap();
+    }
+
+    private void GetAllHiddenItemInMap()
+    {
+        hiddenItemInMap.Clear();
+
+        foreach (Transform obj in transform)
+        {
+            var hiddenItem = obj.GetComponent<HiddenItem>();
+            if (hiddenItem == null) continue;
+
+            hiddenItemInMap.Add(hiddenItem);
+        }
+    }
 }
