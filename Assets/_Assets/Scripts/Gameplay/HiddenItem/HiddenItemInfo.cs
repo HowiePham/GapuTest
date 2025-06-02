06e1bad92b6c6e4a017056e9a3cc0a95528d1efd
user <anonymous@gmail.com>
2025-06-02 16:50:23 +0700

    - Add hidden item and hidden item info

---
+++ Assets/_Assets/Scripts/Gameplay/HiddenItem/HiddenItemInfo.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct HiddenItemInfo
+{
+    public int ItemID;
+    public bool IsFound;
+}
---
+++ Assets/_Assets/Scripts/Gameplay/Map/MapItemManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItemManager : MonoBehaviour
+{
+    [SerializeField] private List<HiddenItem> hiddenItemInMap = new List<HiddenItem>();
+}
