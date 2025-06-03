44a587d578ce0631be626d1516dc387c375fd146
user <anonymous@gmail.com>
2025-06-03 09:35:34 +0700

    - Add Item Progress

---
+++ Assets/_Assets/Scripts/Gameplay/Map/ItemProgress.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class ItemProgress
+{
+    private Dictionary<int, List<HiddenItem>> _allItemInMap = new Dictionary<int, List<HiddenItem>>();
+    private Dictionary<int, int> _numberOfItem = new Dictionary<int, int>();
+    private Dictionary<int, int> _numberOfFoundItem = new Dictionary<int, int>();
+
+    public Dictionary<int, List<HiddenItem>> GetAllItemInMap()
+    {
+        return _allItemInMap;
+    }
+
+    public Dictionary<int, int> GetItemList()
+    {
+        return _numberOfItem;
+    }
+
+    public int GetNumberOfFoundItem(int itemID)
+    {
+        var firstValue = 0;
+        return _numberOfFoundItem.ContainsKey(itemID) ? _numberOfFoundItem[itemID] : firstValue;
+    }
+
+    public int GetNumberOfItem(int itemID)
+    {
+        return _numberOfItem[itemID];
+    }
+
+    public void IncreaseFoundItem(int itemID)
+    {
+        var itemCount = 0;
+        if (_numberOfFoundItem.ContainsKey(itemID)) itemCount = _numberOfFoundItem[itemID];
+        itemCount++;
+        _numberOfFoundItem[itemID] = itemCount;
+
+        InvokeUpdatingItemProgress(itemID);
+    }
+
+    private void InvokeUpdatingItemProgress(int itemID)
+    {
+        GameEventSystem.Invoke(EventName.UpdatingItemProgress, itemID);
+    }
+
+    private void InitNumberOfHiddenItemInMap(List<HiddenItem> hiddenItemInMap)
+    {
+        foreach (var hiddenItem in hiddenItemInMap)
+        {
+            var itemCount = 0;
+            var itemID = hiddenItem.HiddenItemID;
+            if (_numberOfItem.ContainsKey(itemID)) itemCount = _numberOfItem[itemID];
+            itemCount++;
+            _numberOfItem[itemID] = itemCount;
+        }
+    }
+
+    public void InitAllHiddenItem(List<MapController> mapList)
+    {
+        foreach (var map in mapList)
+        {
+            var hiddenItemInMap = map.GetAllHiddenItemInMap();
+            var mapID = map.MapID;
+
+            _allItemInMap[mapID] = hiddenItemInMap;
+
+            InitNumberOfHiddenItemInMap(hiddenItemInMap);
+        }
+    }
+}
--- Assets/_Assets/Scripts/Gameplay/Map/MapProgress.cs
+++ Assets/_Assets/Scripts/Gameplay/Map/MapProgress.cs
@@ -8,48 +8,22 @@ public class MapProgress
     [SerializeField] private int totalFoundItem;
     [SerializeField] private int totalItem;
     [SerializeField] private int currentMapProgress;
-    private Dictionary<int, List<HiddenItem>> _allItemInMap = new Dictionary<int, List<HiddenItem>>();
-    private Dictionary<int, int> _numberOfItem = new Dictionary<int, int>();
-    private Dictionary<int, int> _numberOfFoundItem = new Dictionary<int, int>();
+    [SerializeField] private ItemProgress itemProgress;
 
     public void Init(List<MapController> mapList)
     {
-        InitAllHiddenItem(mapList);
+        itemProgress.InitAllHiddenItem(mapList);
         GetNewTotalItem();
     }
 
     private void GetNewTotalItem()
     {
         totalItem = 0;
+        var allItemInMap = GetAllItemInMap();
         for (var i = 0; i <= currentMapProgress; i++)
         {
-            var hiddenItemList = _allItemInMap[i];
+            var hiddenItemList = allItemInMap[i];
             totalItem += hiddenItemList.Count;
-        }
-    }
-
-    private void InitAllHiddenItem(List<MapController> mapList)
-    {
-        foreach (var map in mapList)
-        {
-            var hiddenItemInMap = map.GetAllHiddenItemInMap();
-            var mapID = map.MapID;
-
-            _allItemInMap[mapID] = hiddenItemInMap;
-
-            InitNumberOfHiddenItemInMap(hiddenItemInMap);
-        }
-    }
-
-    private void InitNumberOfHiddenItemInMap(List<HiddenItem> hiddenItemInMap)
-    {
-        foreach (var hiddenItem in hiddenItemInMap)
-        {
-            var itemCount = 0;
-            var itemID = hiddenItem.HiddenItemID;
-            if (_numberOfItem.ContainsKey(itemID)) itemCount = _numberOfItem[itemID];
-            itemCount++;
-            _numberOfItem[itemID] = itemCount;
         }
     }
 
@@ -80,44 +54,8 @@ public class MapProgress
     {
         totalFoundItem++;
 
-        IncreaseFoundItem(itemID);
+        itemProgress.IncreaseFoundItem(itemID);
         CheckMapProgress();
-    }
-
-    private void IncreaseFoundItem(int itemID)
-    {
-        var itemCount = 0;
-        if (_numberOfFoundItem.ContainsKey(itemID)) itemCount = _numberOfFoundItem[itemID];
-        itemCount++;
-        _numberOfFoundItem[itemID] = itemCount;
-
-        InvokeUpdatingItemProgress(itemID);
-    }
-
-    private void InvokeUpdatingItemProgress(int itemID)
-    {
-        GameEventSystem.Invoke(EventName.UpdatingItemProgress, itemID);
-    }
-
-    public Dictionary<int, List<HiddenItem>> GetAllItemInMap()
-    {
-        return _allItemInMap;
-    }
-
-    public Dictionary<int, int> GetItemList()
-    {
-        return _numberOfItem;
-    }
-
-    public int GetNumberOfFoundItem(int itemID)
-    {
-        var firstValue = 0;
-        return _numberOfFoundItem.ContainsKey(itemID) ? _numberOfFoundItem[itemID] : firstValue;
-    }
-
-    public int GetNumberOfItem(int itemID)
-    {
-        return _numberOfItem[itemID];
     }
 
     public int GetTotalFoundItem()
@@ -133,5 +71,25 @@ public class MapProgress
     public int GetTotalItem()
     {
         return totalItem;
+    }
+
+    public Dictionary<int, List<HiddenItem>> GetAllItemInMap()
+    {
+        return itemProgress.GetAllItemInMap();
+    }
+
+    public Dictionary<int, int> GetItemList()
+    {
+        return itemProgress.GetItemList();
+    }
+
+    public int GetNumberOfFoundItem(int itemID)
+    {
+        return itemProgress.GetNumberOfFoundItem(itemID);
+    }
+
+    public int GetNumberOfItem(int itemID)
+    {
+        return itemProgress.GetNumberOfItem(itemID);
     }
 }
