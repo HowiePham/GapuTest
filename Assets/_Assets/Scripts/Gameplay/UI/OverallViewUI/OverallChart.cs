d61f3606f2e8d3bca8d1993b15313434537ca6b2
user <anonymous@gmail.com>
2025-06-04 17:01:36 +0700

    - Add changing value effect

--- Assets/_Assets/Scripts/Gameplay/UI/OverallViewUI/OverallChart.cs
+++ Assets/_Assets/Scripts/Gameplay/UI/OverallViewUI/OverallChart.cs
@@ -1,13 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OverallChart : MonoBehaviour
 {
     [SerializeField] private Image chartImage;
+    [SerializeField] private float changingTime;
 
     public void UpdateChartUI(int currentCount, int maxCount)
     {
         var fillAmount = (float)currentCount / maxCount;
-        chartImage.fillAmount = fillAmount;
+        StartCoroutine(RunEffect(fillAmount));
+    }
+
+    private IEnumerator RunEffect(float targetVal)
+    {
+        var oldVal = chartImage.fillAmount;
+        float elapsedTime = 0;
+
+        while (elapsedTime < changingTime)
+        {
+            elapsedTime += Time.deltaTime;
+            chartImage.fillAmount = Mathf.Lerp(oldVal, targetVal, elapsedTime / changingTime);
+
+            yield return null;
+        }
     }
 }
