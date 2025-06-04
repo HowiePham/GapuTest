dbb0696b3ce305278500d2672d768e1292f09456
user <anonymous@gmail.com>
2025-06-04 11:48:19 +0700

    - Handle compass tool duration

--- Assets/_Assets/Scripts/Gameplay/GameTool/CompassTool.cs
+++ Assets/_Assets/Scripts/Gameplay/GameTool/CompassTool.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 public class CompassTool : GameTool
 {
     [SerializeField] private Compass compass;
+    [SerializeField] private float toolDuration;
+    [SerializeField] private bool usingTool;
+    private float _toolTimer;
 
     public override void Execute()
+    {
+        InitCompass();
+        SetUsingToolState(true);
+    }
+
+    private void InitCompass()
     {
         var toolType = GetToolType();
         compass.Init(toolType);
@@ -14,6 +23,32 @@ public class CompassTool : GameTool
 
     private void Update()
     {
+        RunToolTimer();
+    }
+
+    private void RunToolTimer()
+    {
+        if (!usingTool) return;
+
+        if (_toolTimer >= toolDuration)
+        {
+            ResetTool();
+            return;
+        }
+
+        _toolTimer += Time.deltaTime;
+    }
+
+    private void ResetTool()
+    {
+        SetUsingToolState(false);
+        _toolTimer = 0;
+        EnableCompass(false);
+    }
+
+    private void SetUsingToolState(bool state)
+    {
+        usingTool = state;
     }
 
     private void EnableCompass(bool state)
